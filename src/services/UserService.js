e6585fe1da1d02f38726718515b5e3d61ff6a9dd
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2023-02-22 15:24:00 +0700

    Đã sữa được một tí

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from '../axios';
 
 const handleLoginApi = (email,password) => {
-    return axios.post('./api/login',{email,password})
+    return axios.post('/api/login',{email,password})
 }
 
 export {
